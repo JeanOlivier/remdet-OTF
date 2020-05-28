1f0695b9e3747516ed6fd38be179fcfb8e34f749
JeanOlivier <jean.olivier.simoneau@usherbrooke.ca>
2020-05-28 15:32:11 -0400

    Added deldet Python Interface

--- src/remdet_wrapper.cpp
+++ src/remdet_wrapper.cpp
@@ -20,10 +20,11 @@ void declare_functions(py::module &m) {
             {sizeof(T),},   // C-style contiguous strides for double
             detpart,        // the data pointer
             free_when_done);// numpy array references this parent
-       }
+        }
     );
     m.def("getdet", [](py::array_t<T, py::array::c_style>& array, uint64_t period) {
         auto buff = array.request();
+
         T *detpart = new T [period]();
         py::capsule free_when_done(detpart, [](void *f) {
             T *detpart = reinterpret_cast<T *>(f);
@@ -35,12 +36,20 @@ void declare_functions(py::module &m) {
             {sizeof(T),},   // C-style contiguous strides for double
             detpart,        // the data pointer
             free_when_done);// numpy array references this parent
-       }
+        }
     );  
+    m.def("deldet", [](py::array_t<T, py::array::c_style>& array, 
+                       py::array_t<T, py::array::c_style>& detpart) {
+        auto buff = array.request();
+        auto dpart = detpart.request();
+        deldet((T*)buff.ptr, buff.size, (T*)dpart.ptr, dpart.size);
+        }
+    );
+
 }
 
 PYBIND11_MODULE(remdet_wrapper, m) {
-    m.doc() = "pybind11 wrapper for remdet"; // optional module docstring
+    m.doc() = "pybind11 wrapper for acorrs.h"; // optional module docstring
     m.attr("the_answer") = 42;
     m.def("set_mpreal_precision", &set_mpreal_precision);
     m.def("set_num_threads", &omp_set_num_threads);
@@ -52,3 +61,4 @@ PYBIND11_MODULE(remdet_wrapper, m) {
     declare_functions<int16_t>(m);
     declare_functions<uint16_t>(m);
 }
+
